c0d8aeff27de2e72a98f33541ebcc53747e7279a
sherlly <jacob@sherllymail.com>
2023-08-27 00:07:47 +0100

    removed hunter and added neotree

---
+++ .config/nvim/after/plugin/neotree.lua
@@ -0,0 +1,3 @@
+vim.keymap.set("n","<Leader>n", '<Cmd>Neotree toggle<CR>')
+
+
--- .config/nvim/lua/sherlly/packer.lua
+++ .config/nvim/lua/sherlly/packer.lua
@@ -18,6 +18,17 @@ return require('packer').startup(function(use)
   use('theprimeagen/harpoon')
   use('mbbill/undotree')
   use('tpope/vim-fugitive')
+  -- animation
+  use('echasnovski/mini.nvim')
+  use {
+  "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    requires = { 
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    }
+  }
   use {
 	  'VonHeikemen/lsp-zero.nvim',
 	  branch = 'v1.x',
